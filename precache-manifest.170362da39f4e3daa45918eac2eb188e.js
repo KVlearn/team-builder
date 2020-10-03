e510b1e55bd1ea231d4e7fb4bf164fcbc1f6a0f0
KVlearn <kavya.mdu@gmail.com>
2020-10-02 22:37:22 -0700

    Updates

---
+++ precache-manifest.170362da39f4e3daa45918eac2eb188e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf87c649e739a2a78954c70e433d1a6",
+    "url": "/team-builder/index.html"
+  },
+  {
+    "revision": "4b6bf41590a357d3814f",
+    "url": "/team-builder/static/css/main.f4108f36.chunk.css"
+  },
+  {
+    "revision": "4c1af8c8ca4bd78490da",
+    "url": "/team-builder/static/js/2.c067e3db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/team-builder/static/js/2.c067e3db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6bf41590a357d3814f",
+    "url": "/team-builder/static/js/main.add9973e.chunk.js"
+  },
+  {
+    "revision": "9780fbe613e826e98997",
+    "url": "/team-builder/static/js/runtime-main.613dfac9.js"
+  }
+]);
